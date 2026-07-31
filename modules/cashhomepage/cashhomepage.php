f817a3e8ce9c3d96d7c5136c1f21d72a91802acc
Jerome Mayran <jmayran@aemis.fr>
2026-07-31 16:09:50 +0200

    Refonte du carrousel des catalogues

--- modules/cashhomepage/cashhomepage.php
+++ modules/cashhomepage/cashhomepage.php
@@ -13,7 +13,7 @@ class CashHomepage extends Module
     {
         $this->name = 'cashhomepage';
         $this->tab = 'front_office_features';
-        $this->version = '1.7.1';
+        $this->version = '1.8.0';
         $this->author = 'Cash Alimentaire';
         $this->need_instance = 0;
         $this->bootstrap = true;
