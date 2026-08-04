d401de6865dfe52b1fffb06dd503e07c4304f176
Jerome Mayran <jmayran@aemis.fr>
2026-08-04 16:29:09 +0200

    Finalise alignement editorial a propos

--- modules/cashhomepage/cashhomepage.php
+++ modules/cashhomepage/cashhomepage.php
@@ -13,7 +13,7 @@ class CashHomepage extends Module
     {
         $this->name = 'cashhomepage';
         $this->tab = 'front_office_features';
-        $this->version = '1.17.1';
+        $this->version = '1.17.2';
         $this->author = 'Cash Alimentaire';
         $this->need_instance = 0;
         $this->bootstrap = true;
