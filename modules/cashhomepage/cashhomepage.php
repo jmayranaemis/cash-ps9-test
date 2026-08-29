71b878adaa8423ec4ee43e7f4fc6e4625ccf15f7
Jerome Mayran <jmayran@aemis.fr>
2026-08-29 19:05:13 +0200

    fix: centrer les filtres alphabétiques des marques

--- modules/cashhomepage/cashhomepage.php
+++ modules/cashhomepage/cashhomepage.php
@@ -1561,7 +1561,7 @@ HTML;
         $this->context->controller->registerStylesheet(
             'module-cashhomepage-header-palette',
             'modules/' . $this->name . '/views/css/header-palette.css',
-            ['media' => 'all', 'priority' => 250, 'version' => $this->version . '-footer-1']
+            ['media' => 'all', 'priority' => 250, 'version' => $this->version . '-footer-2']
         );
 
         $isHomepage = 'index' === $this->context->controller->php_self;
