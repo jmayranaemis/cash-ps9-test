cd2ccfc467165f2b3b1bc61d87f86e79a9a63514
Jerome Mayran <jmayran@aemis.fr>
2026-09-22 20:38:04 +0200

    fix: uniformiser les cartes produit des modules

--- modules/cashhomepage/cashhomepage.php
+++ modules/cashhomepage/cashhomepage.php
@@ -1685,7 +1685,7 @@ HTML;
         $this->context->controller->registerStylesheet(
             'module-cashhomepage',
             'modules/' . $this->name . '/views/css/home.css',
-            ['media' => 'all', 'priority' => 200, 'version' => $this->version . '-3']
+            ['media' => 'all', 'priority' => 200, 'version' => $this->version . '-7']
         );
         if ($isProductsPage) {
             return;
